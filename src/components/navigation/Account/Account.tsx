dfcfbc128b65a88c703b0dbfa73e2079225b66d1
Amitgupta0001 <Amitgupta001503@gmail.com>
2026-08-02 15:16:02 +0530

    feat(account): implement account navigation component

--- src/components/navigation/Account/Account.tsx
+++ src/components/navigation/Account/Account.tsx
@@ -1,7 +1,30 @@
+import "./Account.css";
+
+import { User } from "lucide-react";
+
 function Account() {
     return (
-        <div>
-            Account
+        <div
+            className="account"
+            role="button"
+            tabIndex={0}
+            aria-label="Account and Lists"
+        >
+            <div className="account__icon">
+                <User size={20} />
+            </div>
+
+            <div className="account__content">
+
+                <span className="account__label">
+                    Hello, Sign in
+                </span>
+
+                <span className="account__title">
+                    Account & Lists
+                </span>
+
+            </div>
         </div>
     );
 }

90de8820007920887f8f6ec71e70eabe67f016e5
Amitgupta0001 <Amitgupta001503@gmail.com>
2026-08-02 15:19:05 +0530

    feat(cart): implement shopping cart navigation

--- src/components/navigation/Cart/Cart.tsx
+++ src/components/navigation/Cart/Cart.tsx
@@ -1,7 +1,28 @@
+import "./Cart.css";
+
+import { ShoppingCart } from "lucide-react";
+
 function Cart() {
     return (
-        <div>
-            🛒 0 Cart
+        <div
+            className="cart"
+            role="button"
+            tabIndex={0}
+            aria-label="Shopping Cart"
+        >
+            <div className="cart__icon">
+
+                <ShoppingCart size={22} />
+
+                <span className="cart__count">
+                    0
+                </span>
+
+            </div>
+
+            <span className="cart__text">
+                Cart
+            </span>
         </div>
     );
 }

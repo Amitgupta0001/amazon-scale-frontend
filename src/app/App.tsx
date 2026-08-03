46cee7257c79f1c2e9956ff4b0b0059f6a584489
Amitgupta0001 <Amitgupta001503@gmail.com>
2026-08-03 22:40:14 +0530

    Refactoring the structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/layout/Header";
-import Home from "./pages/Home";
-import Footer from "./components/layout/Footer";
+import Header from "../components/layout/Header";
+import Home from "../pages/Home";
+import Footer from "../components/layout/Footer";
 import "./App.css";
 
 function App() {
---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import type { ReactNode } from "react";
+
+
+type AppProviderProps = {children:ReactNode;};
+
+function AppProviders({children}:AppProviderProps){
+    return (
+        <BrowserRouter>
+        {children}
+        </BrowserRouter>
+    );
+}
+
+export default AppProviders;
